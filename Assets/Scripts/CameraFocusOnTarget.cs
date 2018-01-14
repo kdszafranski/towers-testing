4030145a474820c0216811132909e14d2824fbcc
Kris Szafranski <kris@thoughtshelter.com>
2018-01-14 12:03:14 -0600

    camera how always points at player

---
+++ Assets/Scripts/CameraFocusOnTarget.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFocusOnTarget : MonoBehaviour {
+
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+        if(target) {
+            transform.LookAt(target.transform.position);
+        }	
+	}
+
+    private void FixedUpdate() {
+        if(target) {
+            transform.LookAt(target.transform.position);
+        }
+    }
+}

5c4528f27d1e7284fa2f8173eb4e6cd12c423cda
Kris Szafranski <kris@thoughtshelter.com>
2018-01-14 12:02:58 -0600

    arrows destroy when stopped, now use gravity

---
+++ Assets/Scripts/TowerArrow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerArrow : MonoBehaviour {
+
+    Rigidbody rb;
+    public float damageDealt;
+
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if(rb) {
+            if(rb.velocity.magnitude <= 0) {
+                Destroy(gameObject);
+            }
+        }
+	}
+}

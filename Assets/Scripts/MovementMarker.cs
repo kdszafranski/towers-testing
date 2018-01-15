571c5ff4d14f9ca5d2b836fb0749b4126b886806
Kris Szafranski <kris@thoughtshelter.com>
2018-01-15 12:51:17 -0600

    changed player movement to be based on clicking locations with visual marker.

---
+++ Assets/Scripts/MovementMarker.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementMarker : MonoBehaviour {
+    
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0, 5f, 0);
+	}
+}
--- Assets/Scripts/PlayerMoveController.cs
+++ Assets/Scripts/PlayerMoveController.cs
@@ -6,18 +6,19 @@ public class PlayerMoveController : MonoBehaviour {
 
     Rigidbody rBody;
     public float maxVelocity = 30f;
+    Vector3 currentMoveToLocation;
+    public GameObject movementMarker;
 
 	// Use this for initialization
 	void Start () {
         rBody = gameObject.GetComponent<Rigidbody>();
+        currentMoveToLocation = gameObject.transform.position;
+        movementMarker.SetActive(false);
 	}
 
     // Update is called once per frame
     void FixedUpdate() {
         if(rBody != null) {
-            // -1 to 1, used as a multiplier below
-            //float vertical = Input.GetAxis("Vertical");
-            // control max velocity
 
             // point at and go where we clicked
             if(Input.GetMouseButton(0)) {
@@ -29,20 +30,32 @@ public class PlayerMoveController : MonoBehaviour {
                     Debug.Log(hit.transform.name);
 
                     if (rBody.velocity.magnitude < maxVelocity) {
-                        // face where we clicked
-                        gameObject.transform.LookAt(hit.point);
+                        // set this as our current goal position
+                        currentMoveToLocation = hit.point;
 
-                        // force pushes us forward (whichever direction gameObject is facing)
-                        rBody.AddForce(new Vector3(transform.forward.x, 0, transform.forward.z) * 2500f);
+                        // show visual location marker
+                        movementMarker.transform.position = new Vector3(currentMoveToLocation.x, currentMoveToLocation.y + 5f, currentMoveToLocation.z);
+                        movementMarker.SetActive(true);
                     }
                 }
             }
 
-            //float horizontal = Input.GetAxis("Horizontal");
-
-            //// rotate us around the Y (Vector3.up) axis
-            //transform.Rotate(0, 2f * horizontal, 0);
-
+            if(currentMoveToLocation != gameObject.transform.position) {
+                Move();
+            } else {
+                movementMarker.SetActive(false);
+            }
         }
 	}
+
+    void Move() {
+        gameObject.transform.LookAt(movementMarker.transform.position);
+
+        // whether we clicked or not, move toward our current target position
+        if (currentMoveToLocation != null) {
+            // force pushes us forward (whichever direction gameObject is facing)
+            rBody.AddForce(new Vector3(transform.forward.x, 0, transform.forward.z) * 2500f);
+        }
+
+    }
 }

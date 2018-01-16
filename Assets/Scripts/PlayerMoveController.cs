d78ef1f381768275f21f0342d77821ec904972f0
Kris Szafranski <kris@thoughtshelter.com>
2018-01-16 12:49:42 -0600

    fixed location checking. now matches target x and y rounded values and compares. now works and stops when arrived

--- Assets/Scripts/PlayerMoveController.cs
+++ Assets/Scripts/PlayerMoveController.cs
@@ -6,7 +6,7 @@ public class PlayerMoveController : MonoBehaviour {
 
     Rigidbody rBody;
     public float maxVelocity = 30f;
-    Vector3 currentMoveToLocation;
+    public Vector3 currentMoveToLocation; // should not be public
     public GameObject movementMarker;
 
 	// Use this for initialization
@@ -31,29 +31,50 @@ public class PlayerMoveController : MonoBehaviour {
 
                     if (rBody.velocity.magnitude < maxVelocity) {
                         // set this as our current goal position
-                        currentMoveToLocation = hit.point;
+                        currentMoveToLocation = new Vector3(hit.point.x, gameObject.transform.position.y, hit.point.z);
 
                         // show visual location marker
                         movementMarker.transform.position = new Vector3(currentMoveToLocation.x, currentMoveToLocation.y + 5f, currentMoveToLocation.z);
                         movementMarker.SetActive(true);
                     }
                 }
-            }
+            } // end mouse down
 
-            // TODO need a better way to check they have arrived at the correct spot and not keep spinning.
-            if(currentMoveToLocation != gameObject.transform.position) {
-                Move();
+            // have we arrived at the target location?
+            if (HasArrivedAtTargetLocation()) {
+                // arrived, reset marker
+                if(movementMarker.activeSelf) {
+                    Debug.Log("Arrived");
+                    movementMarker.SetActive(false);
+                }
             } else {
-                movementMarker.SetActive(false);
+                // no, keep on movin'
+                Move();
             }
+        
         }
 	}
 
-    void Move() {
-        gameObject.transform.LookAt(movementMarker.transform.position);
+    bool HasArrivedAtTargetLocation() {
+        // get current values of player object and target position
+        float x = Mathf.Round(gameObject.transform.position.x);
+        float z = Mathf.Round(gameObject.transform.position.z);
+        float targetX = Mathf.Round(currentMoveToLocation.x);
+        float targetZ = Mathf.Round(currentMoveToLocation.z);
 
+        // ignore y (vertical height)
+        if(targetX == x && targetZ == z) {
+            // we have arrived
+            return true;
+        }
+
+        return false;
+    }
+
+    void Move() {
         // whether we clicked or not, move toward our current target position
         if (currentMoveToLocation != null) {
+            gameObject.transform.LookAt(currentMoveToLocation);
             // force pushes us forward (whichever direction gameObject is facing)
             rBody.AddForce(new Vector3(transform.forward.x, 0, transform.forward.z) * 2500f);
         }
